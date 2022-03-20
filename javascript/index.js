c47a77e44cb4e17b840263f87a48832a4dc77a87
BZIEO <100628007+BZIEO@users.noreply.github.com>
2022-03-20 17:39:34 -0500

    Section 4 practice problems

--- javascript/index.js
+++ javascript/index.js
@@ -16,3 +16,41 @@ function random(number) {
 const a = function getRandom() {
   return Math.random();
 };
+
+// Function min(a,b)//
+function min(a, b) {
+  const min = a > b ? b : a;
+  return min;
+}
+
+min(6, 5);
+min(10, 9);
+
+console.log(min(8, 9));
+
+// Function pow(x,n) //
+function pow(x, n) {
+  return x ^ n;
+}
+
+function sayHi() {
+  alert("Hello");
+}
+
+//Fundamentals Part 3 Practice Problems//
+function add7(n) {
+  const num = n + 7;
+  console.log(num);
+}
+
+function multiply(a, b) {
+  console.log(a * b);
+}
+
+function capitalize(string) {
+  console.log(string.charAt(0).toUpperCase() + string.slice(1).toLowerCase());
+}
+
+function lastLetter(string) {
+  console.log(string.slice(-1));
+}
